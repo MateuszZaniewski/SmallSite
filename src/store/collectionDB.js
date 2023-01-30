fee58db5f21dd2cda0a097fce9d9fb9523f6900f
MateuszZaniewski <mateusz.zaniewski94@gmail.com>
2023-01-30 19:15:21 +0100

    working on database

--- src/store/collectionDB.js
+++ src/store/collectionDB.js
@@ -2,24 +2,45 @@ export default {
     collection : {
         item1 : {
             name: 'Olivia 1005',
-            type: 'Set',
+            type: 'Komplet',
             img: require('../store/database/olivia_1005.jpg')
         },
         item2 : {
             name: 'Elise 1003',
-            type: 'Set',
+            type: 'Komplet',
             img: require('../store/database/Elise_1003.jpg')
         },
         item3 : {
             name: 'Melanie 1007',
-            type: 'Set',
+            type: 'Komplet',
             img: require('../store/database/Melanie_1007.jpg')
         },
         item4 : {
             name: 'Jennifer 1013',
-            type: 'Set',
+            type: 'Komplet',
             img: require('../store/database/Jennifer_1013.jpg')
+        },
+        item5 : {
+            name: 'Juliet 1004',
+            type: 'Komplet',
+            img: require('../store/database/Juliet_1004.jpg')
+        },
+        item6 : {
+            name: 'Jasmine 1009',
+            type: 'Komplet',
+            img: require('../store/database/Jasmine_1009.jpg')
+        },
+        item7 : {
+            name: 'Lily 1014',
+            type: 'Komplet',
+            img: require('../store/database/Lily_1014.jpg')
+        },
+        item8 : {
+            name: 'Vivisence 1025',
+            type: 'Komplet',
+            img: require('../store/database/Vivisence_1025.jpg')
         }
+
     }
         
     }
